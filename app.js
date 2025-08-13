8a5e94955e5871108719d735260526286d20e6f9
jhonDevHub <140937630+jhonDevHub@users.noreply.github.com>
2025-08-12 21:23:40 -0500

    Primera versión del challenge Amigo Secreto

---
+++ app.js
@@ -0,0 +1,48 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos=[];
+
+function agregarAmigo() {
+    let valorEntrada = document.getElementById("amigo");
+    let nombre = valorEntrada.value.trim();
+   
+    if (nombre===""){
+        alert("Ingrese un nombre válido");
+        return;
+    }
+    amigos.push(nombre);
+
+    valorEntrada.value ="";
+    valorEntrada.focus();
+    console.log(amigos);
+
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    let lista =document.getElementById("listaAmigos");
+    lista.innerHTML="";
+
+    for (let i=0; i<amigos.length;i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length ===0){
+        document.getElementById("resultado").innerHTML="Lista vacía, no hay amigos para sortear";
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random()*amigos.length);
+
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML=`El amigo sorteado es:<strong> ${amigoSorteado}<strong/>`;
+
+    //amigos.splice(indiceAleatorio,1);
+
+    actualizarListaAmigos();
+}
+
